1a9c80a043a85cf2dc972be88fa63ad962abdd0f
user <nkreivans@gmail.com>
2018-10-02 16:07:53 +0300

    20181002_16_05

---
+++ test_2_20181002.py
@@ -0,0 +1,104 @@
+"""
+x = 4
+if x> 2:
+    print('Bigger')
+else:
+    print("Smaller")
+
+print ("All done")
+
+
+
+
+x=42
+if x>1 :
+    print("More than one")
+    if x < 100 :
+        print("Less than 100")
+    print("Less than 100")
+
+
+ 
+x=5
+if x>2 :
+    print('Bigger than 2')
+    print("Still bigger")
+print("Done with 2")
+
+for i in range(5) :
+    print(i)
+    if i >2 :
+        print("Bigger than 2")
+    print("Done with i", i)
+print("All Done")
+
+
+
+x= 5
+print('Before 5')
+
+if  x == 5 :
+    print('Is 5')
+    print('Is Still 5')
+    print('Third 5')
+print('Afterwards 5')
+print('Before 6')
+if x == 6 :
+    print('Is 6')
+    print('Is Still 6')
+    print('Third 6')
+print('Afterwards 6')
+
+
+
+x=420
+
+if x < 2 :
+    print('small')
+elif x < 10 :
+    print('Medium')
+else:
+    print('LARGE')
+print('All done')
+
+
+
+
+x = 5
+if x <2 :
+    print("small")
+elif x < 10 :
+    print ("Medium")
+else :
+    print ("LARGE")
+print("All done")
+
+
+
+x = 20
+if x < 2 :
+    print('small')
+elif x < 10 :
+    print('Medium')
+else :
+    print('LARGE')
+print('All done')
+
+
+
+x= 5
+if x < 2 :
+    print('Small')
+elif x < 10 :
+    print('Medium')
+print('All done')
+
+"""
+
+astr = "Hello Bob"
+istr = int(astr)
+print("First", istr)
+astr =(astr)
+istr = int(astr)
+print("Second", istr)
+
